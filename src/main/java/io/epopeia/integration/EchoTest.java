eb391578c0730e9e453c8ece10a5be8b9831d1d5
Fernando Amaral <fernandoluizjr@gmail.com>
2019-07-26 18:40:54 -0300

    Update EchoTest.java

--- src/main/java/io/epopeia/integration/EchoTest.java
+++ src/main/java/io/epopeia/integration/EchoTest.java
@@ -5,6 +5,7 @@ import java.time.format.DateTimeFormatter;
 
 import org.jpos.iso.ISOException;
 import org.jpos.iso.ISOMsg;
+import org.jpos.iso.header.BASE1Header;
 import org.jpos.iso.packager.Base1Packager;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Lazy;
@@ -13,8 +14,6 @@ import org.springframework.integration.ip.tcp.TcpSendingMessageHandler;
 import org.springframework.messaging.support.GenericMessage;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
-import org.apache.commons.codec.DecoderException;
-import org.apache.commons.codec.binary.Hex;
 
 @Service
 @Profile("client")
@@ -27,26 +26,37 @@ public class EchoTest {
 	private TcpSendingMessageHandler sender;
 
 	@Scheduled(fixedRate = 5000)
-	public void sendEchoTest() throws ISOException, DecoderException {
+	public void sendEchoTest() throws ISOException {
 
 		// reset stan
-		if (++ stan > 999999) {
+		if (++stan > 999999) {
 			stan = 1;
 		}
 
-		ISOMsg m = new ISOMsg();
+		// create a message container
+		final ISOMsg m = new ISOMsg();
+		m.setPackager(new Base1Packager());
+
+		// set the message fields
 		m.setMTI("0800");
 		m.set(7, LocalDateTime.now().format(DateTimeFormatter.ofPattern("yyMMHHmmss")));
 		m.set(11, String.valueOf(stan));
 		m.set(70, "301");
-		m.setPackager(new Base1Packager());
 
-		final byte[] header = Hex.decodeHex("16010200440000000000000000000000000000000000".toCharArray());
-		byte[] isomsg = m.pack();
-		byte[] fullmsg = new byte[header.length + isomsg.length];
-		System.arraycopy(header, 0, fullmsg, 0, header.length);
-		System.arraycopy(isomsg, 0, fullmsg, header.length, isomsg.length);
+		// get the message buffer
+		final byte[] mb = m.pack();
 
-		sender.handleMessageInternal(new GenericMessage<byte[]>(fullmsg));
+		// get the header buffer
+		final BASE1Header h = new BASE1Header();
+		h.setLen(mb.length);
+		final byte[] hb = h.pack();
+
+		// concatenate the header and message in a buffer
+		final byte[] b = new byte[hb.length + mb.length];
+		System.arraycopy(hb, 0, b, 0, hb.length);
+		System.arraycopy(mb, 0, b, hb.length, mb.length);
+
+		// send the full buffer
+		sender.handleMessageInternal(new GenericMessage<byte[]>(b));
 	}
 }
