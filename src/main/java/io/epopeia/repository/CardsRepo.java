e803acf154ac90a0164005d70840cdf1e350e957
Fernando Amaral <fernandoluizjr@gmail.com>
2019-07-30 16:18:41 -0300

    Update CardsRepo.java

--- src/main/java/io/epopeia/repository/CardsRepo.java
+++ src/main/java/io/epopeia/repository/CardsRepo.java
@@ -1,11 +1,11 @@
 package io.epopeia.repository;
 
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.rest.core.annotation.RestResource;
 
 import io.epopeia.domain.Cards;
 
 @RestResource(path = "cards", rel = "cards")
-public interface CardsRepo extends CrudRepository<Cards, Long> {
+public interface CardsRepo extends JpaRepository<Cards, Long> {
 
 }
