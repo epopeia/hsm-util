fb9aca520b23ef34bb0dc619a948f3db6ac00bbb
Fernando Amaral <fernandoluizjr@gmail.com>
2019-07-30 20:40:48 -0300

    Create IssuersEditor.java

---
+++ src/main/java/io/epopeia/ui/IssuersEditor.java
@@ -0,0 +1,104 @@
+package io.epopeia.ui;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.vaadin.flow.component.Key;
+import com.vaadin.flow.component.KeyNotifier;
+import com.vaadin.flow.component.button.Button;
+import com.vaadin.flow.component.icon.VaadinIcon;
+import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
+import com.vaadin.flow.component.orderedlayout.VerticalLayout;
+import com.vaadin.flow.component.textfield.TextField;
+import com.vaadin.flow.data.binder.Binder;
+import com.vaadin.flow.spring.annotation.SpringComponent;
+import com.vaadin.flow.spring.annotation.UIScope;
+
+import io.epopeia.domain.Issuers;
+import io.epopeia.repository.IssuersRepo;
+
+@SpringComponent
+@UIScope
+public class IssuersEditor extends VerticalLayout implements KeyNotifier {
+	private static final long serialVersionUID = 1L;
+
+	private IssuersRepo repository;
+	private Issuers entity;
+
+	TextField name = new TextField("Name");
+
+	Button save = new Button("Save", VaadinIcon.CHECK.create());
+	Button cancel = new Button("Cancel");
+	Button delete = new Button("Delete", VaadinIcon.TRASH.create());
+	HorizontalLayout actions = new HorizontalLayout(save, cancel, delete);
+
+	Binder<Issuers> binder = new Binder<>(Issuers.class);
+	private ChangeHandler changeHandler;
+
+	@Autowired
+	public IssuersEditor(IssuersRepo repository) {
+		this.repository = repository;
+
+		add(name, actions);
+
+		// bind using naming convention
+		binder.bindInstanceFields(this);
+
+		// Configure and style components
+		setSpacing(true);
+
+		save.getElement().getThemeList().add("primary");
+		delete.getElement().getThemeList().add("error");
+
+		addKeyPressListener(Key.ENTER, e -> save());
+
+		// wire action buttons to save, delete and reset
+		save.addClickListener(e -> save());
+		delete.addClickListener(e -> delete());
+		cancel.addClickListener(e -> edit(entity));
+		setVisible(false);
+	}
+
+	void delete() {
+		repository.delete(entity);
+		changeHandler.onChange();
+	}
+
+	void save() {
+		repository.save(entity);
+		changeHandler.onChange();
+	}
+
+	public interface ChangeHandler {
+		void onChange();
+	}
+
+	public final void edit(Issuers c) {
+		if (c == null) {
+			setVisible(false);
+			return;
+		}
+		final boolean persisted = c.getId() != null;
+		if (persisted) {
+			// Find fresh entity for editing
+			entity = repository.findById(c.getId()).get();
+		} else {
+			entity = c;
+		}
+		cancel.setVisible(persisted);
+
+		// Bind customer properties to similarly named fields
+		// Could also use annotation or "manual binding" or programmatically
+		// moving values from fields to entities before saving
+		binder.setBean(entity);
+
+		setVisible(true);
+
+		// Focus initially
+		name.focus();
+	}
+
+	public void setChangeHandler(ChangeHandler h) {
+		// ChangeHandler is notified when either save or delete is clicked
+		changeHandler = h;
+	}
+}
