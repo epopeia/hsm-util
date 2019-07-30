d2f883d686a070c80483b971b6a15fd186c3e3cf
Fernando Amaral <fernandoluizjr@gmail.com>
2019-07-30 16:18:43 -0300

    Update CustomersRepo.java

--- src/main/java/io/epopeia/repository/CustomersRepo.java
+++ src/main/java/io/epopeia/repository/CustomersRepo.java
@@ -1,11 +1,11 @@
 package io.epopeia.repository;
 
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.rest.core.annotation.RestResource;
 
 import io.epopeia.domain.Customers;
 
 @RestResource(path = "customers", rel = "customers")
-public interface CustomersRepo extends CrudRepository<Customers, Long> {
+public interface CustomersRepo extends JpaRepository<Customers, Long> {
 
 }
