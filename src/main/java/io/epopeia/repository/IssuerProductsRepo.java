a67a3989c45139c7daf0eca1007deda8b54eb90f
Fernando Amaral <fernandoluizjr@gmail.com>
2019-07-30 16:18:45 -0300

    Update IssuerProductsRepo.java

--- src/main/java/io/epopeia/repository/IssuerProductsRepo.java
+++ src/main/java/io/epopeia/repository/IssuerProductsRepo.java
@@ -1,11 +1,11 @@
 package io.epopeia.repository;
 
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.rest.core.annotation.RestResource;
 
 import io.epopeia.domain.IssuerProducts;
 
 @RestResource(path = "issuerProducts", rel = "issuerProducts")
-public interface IssuerProductsRepo extends CrudRepository<IssuerProducts, Long> {
+public interface IssuerProductsRepo extends JpaRepository<IssuerProducts, Long> {
 
 }
