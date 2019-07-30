170b6b7bce501fb04b885084196a3da2f9e1ea33
Fernando Amaral <fernandoluizjr@gmail.com>
2019-07-30 14:41:07 -0300

    Create Parameters.java

---
+++ src/main/java/io/epopeia/domain/Parameters.java
@@ -0,0 +1,31 @@
+package io.epopeia.domain;
+
+import java.util.Date;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+
+import lombok.Data;
+import lombok.EqualsAndHashCode.Exclude;
+
+@Entity
+public @Data class Parameters {
+
+	@Id
+	private Long id;
+
+	private String name;
+	private String description;
+	private String default_value;
+	private Long active;
+
+	@Exclude
+	@Temporal(TemporalType.TIMESTAMP)
+	private Date created_at = new Date();
+
+	@Exclude
+	@Temporal(TemporalType.TIMESTAMP)
+	private Date updated_at = new Date();
+}
