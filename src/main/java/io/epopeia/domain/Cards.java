1b198e505e81da48bb0dd5b47f181e328c4040b1
Fernando Amaral <fernandoluizjr@gmail.com>
2019-07-30 14:56:00 -0300

    Update Cards.java

--- src/main/java/io/epopeia/domain/Cards.java
+++ src/main/java/io/epopeia/domain/Cards.java
@@ -16,6 +16,7 @@ import lombok.EqualsAndHashCode.Exclude;
 public @Data class Cards {
 
 	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 
 	private String masked_card;
