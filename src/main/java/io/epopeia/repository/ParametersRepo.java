a1415970bf9fef95f905e93dadce87b450750682
Fernando Amaral <fernandoluizjr@gmail.com>
2019-07-30 16:18:52 -0300

    Update ParametersRepo.java

--- src/main/java/io/epopeia/repository/ParametersRepo.java
+++ src/main/java/io/epopeia/repository/ParametersRepo.java
@@ -1,11 +1,11 @@
 package io.epopeia.repository;
 
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.rest.core.annotation.RestResource;
 
 import io.epopeia.domain.Parameters;
 
 @RestResource(path = "parameters", rel = "parameters")
-public interface ParametersRepo extends CrudRepository<Parameters, Long> {
+public interface ParametersRepo extends JpaRepository<Parameters, Long> {
 
 }
