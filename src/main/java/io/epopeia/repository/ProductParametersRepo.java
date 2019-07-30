ab42e17eb1dd4c038c271df20bd65b4407bfc96c
Fernando Amaral <fernandoluizjr@gmail.com>
2019-07-30 16:18:54 -0300

    Update ProductParametersRepo.java

--- src/main/java/io/epopeia/repository/ProductParametersRepo.java
+++ src/main/java/io/epopeia/repository/ProductParametersRepo.java
@@ -1,11 +1,11 @@
 package io.epopeia.repository;
 
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.rest.core.annotation.RestResource;
 
 import io.epopeia.domain.ProductParameters;
 
 @RestResource(path = "productParameters", rel = "productParameters")
-public interface ProductParametersRepo extends CrudRepository<ProductParameters, Long> {
+public interface ProductParametersRepo extends JpaRepository<ProductParameters, Long> {
 
 }
