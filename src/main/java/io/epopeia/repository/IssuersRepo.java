8be66f0fae2ff7ab2ee2ba3eb4e7efba5df364bf
Fernando Amaral <fernandoluizjr@gmail.com>
2019-07-30 16:18:47 -0300

    Update IssuersRepo.java

--- src/main/java/io/epopeia/repository/IssuersRepo.java
+++ src/main/java/io/epopeia/repository/IssuersRepo.java
@@ -1,11 +1,11 @@
 package io.epopeia.repository;
 
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.rest.core.annotation.RestResource;
 
 import io.epopeia.domain.Issuers;
 
 @RestResource(path = "issuers", rel = "issuers")
-public interface IssuersRepo extends CrudRepository<Issuers, Long> {
+public interface IssuersRepo extends JpaRepository<Issuers, Long> {
 
 }
