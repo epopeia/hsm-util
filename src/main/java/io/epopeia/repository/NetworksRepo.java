ca9988a950585547cf974d0c78650ae1c4cf3271
Fernando Amaral <fernandoluizjr@gmail.com>
2019-07-30 16:18:50 -0300

    Update NetworksRepo.java

--- src/main/java/io/epopeia/repository/NetworksRepo.java
+++ src/main/java/io/epopeia/repository/NetworksRepo.java
@@ -1,11 +1,11 @@
 package io.epopeia.repository;
 
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.rest.core.annotation.RestResource;
 
 import io.epopeia.domain.Networks;
 
 @RestResource(path = "networks", rel = "networks")
-public interface NetworksRepo extends CrudRepository<Networks, Long> {
+public interface NetworksRepo extends JpaRepository<Networks, Long> {
 
 }
