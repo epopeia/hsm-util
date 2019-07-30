d67594965ab944a2ccf2934449cc629559c69376
Fernando Amaral <fernandoluizjr@gmail.com>
2019-07-30 16:16:29 -0300

    Update CardProfileParametersRepo.java

--- src/main/java/io/epopeia/repository/CardProfileParametersRepo.java
+++ src/main/java/io/epopeia/repository/CardProfileParametersRepo.java
@@ -6,6 +6,6 @@ import org.springframework.data.rest.core.annotation.RestResource;
 import io.epopeia.domain.CardProfileParameters;
 
 @RestResource(path = "cardProfileParameters", rel = "cardProfileParameters")
-public interface CardProfileParametersRepo extends CrudRepository<CardProfileParameters, Long> {
+public interface CardProfileParametersRepo extends JpaRepository<CardProfileParameters, Long> {
 
 }
