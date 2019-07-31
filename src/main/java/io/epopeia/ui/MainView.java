69965265b6740b7c9ddabc97ec29e54fabd8a98f
Fernando Amaral <fernandoluizjr@gmail.com>
2019-07-31 19:17:52 -0300

    Update MainView.java

--- src/main/java/io/epopeia/ui/MainView.java
+++ src/main/java/io/epopeia/ui/MainView.java
@@ -7,19 +7,34 @@ import com.vaadin.flow.component.applayout.AppLayoutMenuItem;
 import com.vaadin.flow.component.html.H3;
 import com.vaadin.flow.router.Route;
 
+import io.epopeia.ui.layout.CustomersLayout;
 import io.epopeia.ui.layout.IssuersLayout;
 import io.epopeia.ui.layout.NetworksLayout;
+import io.epopeia.ui.layout.ParametersLayout;
+import io.epopeia.ui.layout.ProductsLayout;
 
 @Route
 public class MainView extends AppLayout {
 	private static final long serialVersionUID = 1L;
 
 	@Autowired
-	public MainView(IssuersLayout issuers, NetworksLayout networks) {
+	public MainView(IssuersLayout issuers, NetworksLayout networks, ParametersLayout parameters,
+			CustomersLayout customers, ProductsLayout products) {
 		super();
 		setBranding(new H3("Epopeia Authorizator"));
 
-		createMenu().addMenuItems(new AppLayoutMenuItem("Issuers", e -> setContent(issuers)),
-				new AppLayoutMenuItem("Networks", e -> setContent(networks)));
+		AppLayoutMenuItem mProducts = new AppLayoutMenuItem("Products", e -> setContent(products));
+		AppLayoutMenuItem mCustomers = new AppLayoutMenuItem("Customers", e -> setContent(customers));
+		AppLayoutMenuItem mParameters = new AppLayoutMenuItem("Parameters", e -> setContent(parameters));
+		AppLayoutMenuItem mIssuers = new AppLayoutMenuItem("Issuers", e -> setContent(issuers));
+		AppLayoutMenuItem mNetworks = new AppLayoutMenuItem("Networks", e -> setContent(networks));
+
+		mProducts.addMenuItemClickListener(e -> products.refresh());
+		mCustomers.addMenuItemClickListener(e -> customers.refresh());
+		mParameters.addMenuItemClickListener(e -> parameters.refresh());
+		mIssuers.addMenuItemClickListener(e -> issuers.refresh());
+		mNetworks.addMenuItemClickListener(e -> networks.refresh());
+
+		createMenu().addMenuItems(mProducts, mCustomers, mParameters, mIssuers, mNetworks);
 	}
 }
