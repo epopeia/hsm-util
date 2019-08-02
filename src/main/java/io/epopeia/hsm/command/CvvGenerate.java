b455e4bd4723c5c3c2248a02d4d7f4cc9a1d7fd4
Robson <ia@epopeia.io>
2019-08-01 23:51:12 -0400

    fixes

--- src/main/java/io/epopeia/hsm/command/CvvGenerate.java
+++ src/main/java/io/epopeia/hsm/command/CvvGenerate.java
@@ -2,23 +2,21 @@ package io.epopeia.hsm.command;
 
 import io.epopeia.hsm.HSMResponse;
 
-class CvkValidate extends Command 
+class CvvGenerate extends Command 
 {
 	
 	private String pan;
 	private String cvkKey;
 	private String cardExpirationCavv;
 	private String serviceCode;
-	private String cvv;
 	
-	// CVV1 service code vem da trilha
-	// CVV2 service code é 000
-	// iCVV service code é 999
-	public CvkValidate(String pan, String cvv, int expirationYear, int expirationMonth, String serviceCode, String cvkKey)
+	// servicecode for magstripe
+	// 000 cvv2
+	// 999 icvv
+	public CvvGenerate(String pan, int expirationYear, int expirationMonth, String serviceCode, String cvkKey)
 	{
 		this.pan = pan;
 		this.cvkKey = cvkKey;
-		this.cvv = cvv;
 		this.cardExpirationCavv = String.valueOf(expirationYear) + String.valueOf(expirationMonth);
 		this.serviceCode = serviceCode;
 	}
@@ -26,18 +24,19 @@ class CvkValidate extends Command
 	public HSMResponse execute(String[] args) throws Exception {
 		
 		String header = "0000";
+		
 		StringBuffer sb = new StringBuffer();
 		sb.append(header); //0000 header
-		sb.append("CY"); // command to generate
+		sb.append("CW"); // command to generate
 		sb.append(this.cvkKey);
-		sb.append(this.cvv);
 		sb.append(this.pan); 
 		sb.append(";");
 		sb.append(this.cardExpirationCavv);
 		sb.append(serviceCode);
 		
 		
-		return new HSMResponse( super.runCmd(sb.toString()), header, "CZ");
+		return new HSMResponse( super.runCmd(sb.toString()), header, "CX");
+		
 	}
 
 }
--- src/main/java/io/epopeia/hsm/command/CvvValidate.java
+++ src/main/java/io/epopeia/hsm/command/CvvValidate.java
@@ -2,18 +2,23 @@ package io.epopeia.hsm.command;
 
 import io.epopeia.hsm.HSMResponse;
 
-class CvkGenerate extends Command 
+class CvvValidate extends Command 
 {
 	
 	private String pan;
 	private String cvkKey;
 	private String cardExpirationCavv;
 	private String serviceCode;
+	private String cvv;
 	
-	public CvkGenerate(String pan, int expirationYear, int expirationMonth, String serviceCode, String cvkKey)
+	// CVV1 service code vem da trilha
+	// CVV2 service code é 000
+	// iCVV service code é 999
+	public CvvValidate(String pan, String cvv, int expirationYear, int expirationMonth, String serviceCode, String cvkKey)
 	{
 		this.pan = pan;
 		this.cvkKey = cvkKey;
+		this.cvv = cvv;
 		this.cardExpirationCavv = String.valueOf(expirationYear) + String.valueOf(expirationMonth);
 		this.serviceCode = serviceCode;
 	}
@@ -21,19 +26,18 @@ class CvkGenerate extends Command
 	public HSMResponse execute(String[] args) throws Exception {
 		
 		String header = "0000";
-		
 		StringBuffer sb = new StringBuffer();
 		sb.append(header); //0000 header
-		sb.append("CK"); // command to generate
+		sb.append("CY"); // command to generate
 		sb.append(this.cvkKey);
+		sb.append(this.cvv);
 		sb.append(this.pan); 
 		sb.append(";");
 		sb.append(this.cardExpirationCavv);
 		sb.append(serviceCode);
 		
 		
-		return new CvkHSMResponse( super.runCmd(sb.toString()), header);
-		
+		return new HSMResponse( super.runCmd(sb.toString()), header, "CZ");
 	}
 
 }
