457f0556179981d4aeb953d61205c32b0ca49e89
Fernando Amaral <fernandoluizjr@gmail.com>
2019-08-02 08:33:41 -0300

    Update IssuersLayout.java

--- src/main/java/io/epopeia/ui/layout/IssuersLayout.java
+++ src/main/java/io/epopeia/ui/layout/IssuersLayout.java
@@ -12,16 +12,20 @@ import com.vaadin.flow.spring.annotation.UIScope;
 
 import io.epopeia.domain.Issuers;
 import io.epopeia.repository.IssuersRepo;
-import io.epopeia.ui.IssuersEditor;
+import io.epopeia.ui.form.IssuersEditor;
 
 @SpringComponent
 @UIScope
 public class IssuersLayout extends VerticalLayout {
 	private static final long serialVersionUID = 1L;
 
+	Grid<Issuers> grid = new Grid<>(Issuers.class, false);
+	IssuersRepo repo;
+
 	@Autowired
 	public IssuersLayout(IssuersRepo repo, IssuersEditor form) {
-		Grid<Issuers> grid = new Grid<>(Issuers.class, false);
+		this.repo = repo;
+
 		grid.setHeight("300px");
 		grid.setColumns("name", "created_at", "updated_at");
 
@@ -33,14 +37,19 @@ public class IssuersLayout extends VerticalLayout {
 		// Listen changes made by the editor, refresh data from back-end
 		form.setChangeHandler(() -> {
 			form.setVisible(false);
-			grid.setItems(repo.findAll());
+			refresh();
 		});
 
 		Button addBt = new Button(VaadinIcon.PLUS.create(), e -> form.edit(new Issuers()));
+		addBt.setText("New " + Issuers.class.getSimpleName());
 		HorizontalLayout actions = new HorizontalLayout(addBt);
 		add(actions, grid, form);
 
 		// Initialize listing
+		refresh();
+	}
+
+	public void refresh() {
 		grid.setItems(repo.findAll());
 	}
 }
